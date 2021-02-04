6de196f1a8c61cadd5e14204a2b33611c31f608a
Yasutaka Hanada <hanada1985@gmail.com>
2021-02-04 17:38:21 +0900

    add version info

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # qmapix
 
+nothing here to see yet.
+
+Version 0.1
+
+<!--
 (Advanpix)[https://www.advanpix.com/] を使ったmatlabによる量子カオス系の多倍長計算パッケージ
+-->
--- test_FundamentalBasis.m
+++ test_FundamentalBasis.m
@@ -1,5 +1,5 @@
-import FundamentalBasis
-addpath("/nfs/AdvanpixMCT-4.8.0.14100/");
+addpath("/nfs/AdvanpixMCT-4.8.3.14440/");
+%addpath("/nfs/ResearchData/2020-A3/matlab/qmapix/")
 
 dps = 100;
 
@@ -7,25 +7,72 @@ mp.Digits(dps);
 format longG;
 
 dim = 100;
-%domain = [-pi pi;-2*pi 2*pi];
-domain = mp('[-pi pi; -2*pi, 2*pi]');
-rep = 'q';
-H = FundamentalBasis(dim, domain, rep)
+domain = [-pi pi;-2*pi 2*pi];
+%domain = mp('[-pi pi; -2*pi, 2*pi]');
+basis = 'q';
+H = SplitHamiltonian(dim, domain, basis);
+U = SplitUnitary(dim, domain, basis);
 
-matT = H.matT(@(x) x .* x / 2 );
+matT = H.qBaseMatT(@(x) x .* x / 2 );
 matV = H.matV(@(x) cos(x));
-disp(H.q)
+
+
 matH = matV + matT;
 [evecs, evalsmat] = eig(matH);
 [evals, sindex] = sort(real(diag(evalsmat)));
 evecs = evecs(:,sindex);
-class(evecs)
 
-semilogy(H.p, abs2(evecs(:, 2)))
+vec = evecs(:, 1)
+size(vec)
+semilogy(H.q, abs2(vec))
+return 
+%states = U.eigen2states(evals, evecs);
+%a = states(1).q2p();
+%display(a)
+%b = states(2);
+%y = a + a;
+%y.data
+return 
+%a = state(:,1);
+%state.eigenvalue
+%size(states)
 
-function y = abs2(x)
-y = abs(conj(x) .* x);
+
+%size(diag(evalsmat))
+%utils.save_eigenvalues(H, diag(evalsmat), "test_dir");
+%return 
+
+%vec = evecs(:, 1)
+%U.setop(@T, @V)
+%set(U,'funcV', @(x) x * x)
+%op = U.getop()
+%vec = op(invec)
+%U.evolveTV(invec)
+
+%semilogy(H.q, abs2(vec))
+
+%hold on
+%for i = 1:10
+%    vec = U.evolveTV(vec);
+%    disp(i);
+%    semilogy(H.q, abs2(vec),'-', 'Linewidth',2);
+%end
+%hold off
+%H.test(T)
+%return 
+%norm(evecs(:,1))
+
+
+%semilogy(H.p, abs2(evecs(:, 2)))
+
+function y = T(x)
+y = x .* x / 2;
 end
+
+function y = V(x)
+y = cos(x);
+end
+
 
 
 
